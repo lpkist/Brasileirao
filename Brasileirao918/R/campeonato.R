96692ae7d5860bd254720cef69ee3d954fadb4af
Ana Julia Cunha <137313045+ajcunhw@users.noreply.github.com>
2023-11-15 14:55:09 -0300

    Add files via upload

--- Brasileirao918/R/campeonato.R
+++ Brasileirao918/R/campeonato.R
@@ -1,8 +1,21 @@
 campeonato <- function(){
-tab_campeonato <- column(3,
-                      h1("Campeonato")
+tab_campeonato <- fluidRow(column(2,
+                      h1("Campeonato"),
+                      sliderInput(inputId = "Ano",
+                                  label = "Escolha um Ano:",
+                                  min = 2003, max = 2023,
+                                  value = 2013, step = 1),
+                      tableOutput("top3ArbApi"),
+                      tableOutput("Posicoes8")
+                      ),
+                  column(6,
+                         h3("Evolução dos Times, ao longo das Rodadas")),
+                  column(4,
+
+                         )
                       # Conteúdo do Slide de Modelos aqui
 )
+return(list(tab_campeonato = tab_campeonato))
 }
 
 library(Brasileirao918)
@@ -46,10 +59,22 @@ PubResCamp <- brasileirao %>% filter(ano_campeonato == 2016) %>%
          MinPublico = min(publico, na.rm = T)) %>%
   View()
 
+
+
 ## Arbitro ############
-top3ArbApi <- brasileirao %>%
-  filter(ano_campeonato == 2016) %>%
-  count(arbitro) %>%
-  arrange(-n) %>% top_n(n, n = 3)
+# top3ArbApi <- brasileirao %>%
+#   filter(ano_campeonato == 2016) %>%
+#   count(arbitro) %>%
+#   arrange(-n) %>% top_n(n, n = 3)
 
 ###Colocacao
+M <- brasileirao %>% filter(ano_campeonato == 2016, rodada == 38) %>%
+  select(time_mandante, colocacao_mandante) %>%
+  rename("Time" = "time_mandante", "Posicao" = "colocacao_mandante")
+V <- brasileirao %>% filter(ano_campeonato == 2016, rodada == 38) %>%
+  select(time_visitante, colocacao_visitante) %>%
+  rename("Time" = "time_visitante", "Posicao" = "colocacao_visitante")
+
+posi <- rbind(M, V) %>%
+  filter(!Posicao %in% c(5:16)) %>% arrange(Posicao)
+
