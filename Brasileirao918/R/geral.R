53cb5c9a12c650f90fa9e62f38b04463eed8b0d5
Ana Julia Cunha <137313045+ajcunhw@users.noreply.github.com>
2023-11-25 00:02:45 -0300

    Add files via upload

--- Brasileirao918/R/geral.R
+++ Brasileirao918/R/geral.R
@@ -1,8 +1,18 @@
 geral <- function(){
-tab_geral <- column(12,
-                      h1("Análise Geral"),
-
-                    h2("o fur n trabalha")
-                      # Conteúdo do Slide de Modelos aqui
+tab_geral <- fluidRow(column(3,
+                           h1("Geral"),
+                           tableOutput('Pub_total'),
+                           h3('Estatística de Gols'),
+                           tableOutput('Gols_total'),
+                           tableOutput('Ano_gols_max'),
+                           tableOutput('Ano_gols_min')),
+                    column(6,
+                           h4("Valor do Time ao Longo do Tempo"),
+                           plotlyOutput("valor_times", width = 500, height = 370)),
+                    column(3,
+                           h3("Menor Valor de Time"),
+                           tableOutput('Time_valor_min'),
+                           h3('Maior Valor de Time'),
+                           tableOutput('Time_valor_max'))
 )
 }
