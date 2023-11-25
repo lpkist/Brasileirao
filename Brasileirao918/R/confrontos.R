017874c7314ea88833e8f650d977d618e1ec2d9e
Ana Julia Cunha <137313045+ajcunhw@users.noreply.github.com>
2023-11-25 11:14:52 -0300

    Update confrontos.R

--- Brasileirao918/R/confrontos.R
+++ Brasileirao918/R/confrontos.R
@@ -1,6 +1,5 @@
 confrontos <- function(time1 = "Grêmio", time2 = "Internacional", periodo= c(2003,2023)){
-  #library(tidyverse)
-  tab_confrontos <- column(12,
+  tab_confrontos <- fluidRow(column(5,
                       h1("Confrontos"),
                       selectInput(inputId = "time1",
                                   label = "Escolha o 1º time",
@@ -15,13 +14,13 @@ confrontos <- function(time1 = "Grêmio", time2 = "Internacional", periodo= c(20
                                   min = 2003,
                                   max = max(brasileirao$ano_campeonato),
                                   value = c(2003,2023),
-                                  step = 1, sep = ""),
+                                  step = 1, sep = "")),
+                      column(7,
                       h4("Histórico dos Confrontos"),
                       tableOutput("historico_conf"),
                       tableOutput("gols_conf"),
                       tableOutput("jogos_s_gols")
 
-
-)
+))
 return(list(tab_confrontos = tab_confrontos))
 }
