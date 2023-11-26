cfa2a1e24e8c292eae26b706ce3132f998de98a3
Lucas Perondi Kist <137221454+lpkist@users.noreply.github.com>
2023-11-26 16:05:15 -0300

    Add files via upload

---
+++ Brasileirao918/Brasileirao918/R/arbitro.R
@@ -0,0 +1,27 @@
+arbitro <- function(){
+  suppressPackageStartupMessages(library(shiny))
+  suppressPackageStartupMessages(library(shinydashboard))
+  tab_arbitro <- fluidRow(column(2,
+                                 h1("Árbitro"),
+                                 selectInput(
+                                   inputId = "arbitro",
+                                   label = "Escolha o Árbitro:",
+                                   choices = unique(brasileirao$arbitro)),
+                                 sliderInput(inputId = "AnoArbitro",
+                                             label = "Escolha um Ano:",
+                                             min = 2003, max = 2023,
+                                             value = 2013, step = 1, sep = ""),
+                                 h3("Hitórico Geral(2003 - 2023)"),
+                                 tableOutput("GolsAp"),
+                                 tableOutput("FaltasAp")),
+                          column(7,
+                                 plotOutput("Top5EstAp")),
+                          column(3,
+                                 h3("Análise Por Temporada Escolhida"),
+                                 tableOutput("GolsApTemp"),
+                                 tableOutput("FaltasApTemp"),
+                                 tableOutput("TimesApTemp"))
+  )
+
+  return(list(tab_arbitro = tab_arbitro))
+}
---
+++ Brasileirao918/Brasileirao918/R/confrontos.R
@@ -0,0 +1,26 @@
+confrontos <- function(time1 = "Grêmio", time2 = "Internacional", periodo= c(2003,2023)){
+  tab_confrontos <- fluidRow(column(5,
+                      h1("Confrontos"),
+                      selectInput(inputId = "time1",
+                                  label = "Escolha o 1º time",
+                                  choices = unique(brasileirao$time_visitante),
+                                  "Grêmio"),
+                      selectInput(inputId = "time2",
+                                  label = "Escolha o 2º time",
+                                  choices = unique(brasileirao$time_mandante),
+                                  "Internacional"),
+                      sliderInput("periodo_conf",
+                                  "Selecione o período do confronto",
+                                  min = 2003,
+                                  max = max(brasileirao$ano_campeonato),
+                                  value = c(2003,2023),
+                                  step = 1, sep = "")),
+                      column(7,
+                      h4("Histórico dos Confrontos"),
+                      tableOutput("historico_conf"),
+                      tableOutput("gols_conf"),
+                      tableOutput("jogos_s_gols")
+
+))
+return(list(tab_confrontos = tab_confrontos))
+}
